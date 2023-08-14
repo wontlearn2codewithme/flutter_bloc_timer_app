030b2283da34907e1e516f77566fc8847b1000ee
wontlearn2codewithme <wontlear2codewithme@gmail.com>
2023-08-14 15:38:54 +0200

    first commit

---
+++ lib/timer/bloc/timer_bloc.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../ticker.dart';
+
+part 'timer_event.dart';
+part 'timer_state.dart';
+
+class TimerBloc extends Bloc<TimerEvent, TimerState> {
+  static const int _duration = 60;
+  final Ticker _ticker;
+  StreamSubscription<int>? _tickerSubscription;
+
+  TimerBloc({required Ticker ticker})
+      : _ticker = ticker,
+        super(const TimerInitial(_duration)) {
+    on<TimerStarted>(_onStarted);
+    on<TimerPaused>(_onPaused);
+    on<TimerResumed>(_onResumed);
+    on<TimerReset>(_onReseted);
+    on<_TimerTicked>(_onTicked);
+  }
+
+  @override
+  Future<void> close() {
+    _tickerSubscription?.cancel();
+    return super.close();
+  }
+
+  void _onStarted(TimerStarted event, Emitter<TimerState> emit) {
+    emit(TimerRunInProgress(event.duration));
+    _tickerSubscription?.cancel();
+    _tickerSubscription = _ticker
+        .tick(ticks: event.duration)
+        .listen((event) => add(_TimerTicked(duration: event)));
+  }
+
+  void _onTicked(_TimerTicked event, Emitter<TimerState> emit) {
+    emit(event.duration > 0
+        ? TimerRunInProgress(event.duration)
+        : const TimerRunComplete());
+  }
+
+  void _onPaused(TimerPaused event, Emitter<TimerState> emit) {
+    if (state is TimerRunInProgress) {
+      _tickerSubscription?.pause();
+      emit(TimerRunPause(state.duration));
+    }
+  }
+
+  void _onResumed(TimerResumed event, Emitter<TimerState> emit) {
+    if (state is TimerRunPause) {
+      _tickerSubscription?.resume();
+      emit(TimerRunInProgress(state.duration));
+    }
+  }
+
+  void _onReseted(TimerReset event, Emitter<TimerState> emit) {
+    _tickerSubscription?.cancel();
+    emit(const TimerInitial(_duration));
+  }
+}
---
+++ lib/timer/bloc/timer_event.dart
@@ -0,0 +1,30 @@
+part of 'timer_bloc.dart';
+
+sealed class TimerEvent extends Equatable {
+  const TimerEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class TimerStarted extends TimerEvent {
+  const TimerStarted({required this.duration});
+  final int duration;
+}
+
+final class TimerPaused extends TimerEvent {
+  const TimerPaused();
+}
+
+final class TimerResumed extends TimerEvent {
+  const TimerResumed();
+}
+
+class TimerReset extends TimerEvent {
+  const TimerReset();
+}
+
+class _TimerTicked extends TimerEvent {
+  const _TimerTicked({required this.duration});
+  final int duration;
+}
